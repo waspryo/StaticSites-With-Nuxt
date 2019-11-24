7881a55e7b13f55f47ec51f230ea7d47c4d21876
waspryo <wasp2123321@gmail.com>
2019-11-24 17:48:14 +0900

    basic SEO

---
+++ layouts/contact-layout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+      <h2>contact-layout</h2>
+    <nuxt/>
+    <footer>
+        this is contact-layout footer
+    </footer>
+  </div>
+</template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,3 +1,18 @@
 <template>
   <div>contact page</div>
 </template>
+
+<script>
+export default {
+  layout: "contact-layout",
+  head() {
+    return {
+      title: 'Contact page',
+      meta: [
+        {hid: 'description', name: 'description', content: 'My description'},
+        {hid: 'ogtitle', property: 'og:title', content: 'Contact page'}
+      ]
+    }
+  }
+};
+</script>
